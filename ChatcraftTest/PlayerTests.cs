40ccea990615b40d66d2b8b6b314472df45e098f
Simplevolk <simplevolk@gmail.com>
2017-03-11 01:35:19 +0300

    Подключил Unit Test Templates, Moq фреймворк

---
+++ ChatcraftTest/PlayerTests.cs
@@ -0,0 +1,43 @@
+using Chatcraft;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ChatcraftTest
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+            Player player = this.CreatePlayer();
+
+
+        }
+
+        private Player CreatePlayer()
+        {
+            return new Player();
+        }
+    }
+}
