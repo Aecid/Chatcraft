049d765881a71bf6f37bffc671893ec83e31e315
Simplevolk <simplevolk@gmail.com>
2017-03-13 21:26:07 +0300

    удалил веб проект. добавил поддержку докера

--- Chatcraft.Web/App_Start/FilterConfig.cs
+++ Chatcraft.Web/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Chatcraft.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
