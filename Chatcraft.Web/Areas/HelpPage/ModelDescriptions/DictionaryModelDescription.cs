d0edacd09bf8e1dd84937b3dc7810b1dc3a35726
Simplevolk <simplevolk@gmail.com>
2017-03-19 21:24:07 +0300

    Remove web project

--- Chatcraft.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Chatcraft.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Chatcraft.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chatcraft.Web.WebApiApplication" Language="C#" %>
