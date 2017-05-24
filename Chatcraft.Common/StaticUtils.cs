c444948849535836556692263438ebdb7283d957
Simplevolk <simplevolk@gmail.com>
2017-05-24 19:58:42 +0300

    добавил блоки try catch в класс QuestsPage

--- Chatcraft.Common/StaticUtils.cs
+++ Chatcraft.Common/StaticUtils.cs
@@ -9,11 +9,14 @@ namespace Chatcraft.Common
     public static class StaticUtils
     {
         public static ILoggerFactory LoggerFactory;
+
+        public static ILogger Logger;
         static StaticUtils()
         {
             LoggerFactory = new LoggerFactory();
             LoggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
             LoggerFactory.AddConsole();
+            Logger= StaticUtils.LoggerFactory.CreateLogger<Player>();
         }
          
     }
